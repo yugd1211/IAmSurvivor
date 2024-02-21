258f3ec2ddc90d5d5a692dbfede6a04bd3585446
yurudehd <gyyu@student.42seoul.kr>
2024-02-21 14:12:14 +0900

    FEAT : 업적 추가, 업적에 따른 캐릭터 해금기능 추가

---
+++ Assets/Scripts/AchieveManager.cs
@@ -0,0 +1,96 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AchieveManager : MonoBehaviour
+{
+    public GameObject[] lockCharacter;
+    public GameObject[] unlockCharacter;
+    public GameObject uiNotice;
+    
+    
+    enum Achieve 
+    { 
+        UnlockPotato,
+        UnlockBean, 
+    }
+    private Achieve[] _achieves;
+    private WaitForSecondsRealtime _wait;
+    
+    private void Awake()
+    {
+        _achieves = (Achieve[])Enum.GetValues(typeof(Achieve));
+        _wait = new WaitForSecondsRealtime(5);
+        if (!PlayerPrefs.HasKey("InitAchieve"))
+            Init();
+    }
+
+    private void Start()
+    {
+        UnlockCharacter();
+    }
+
+    void LateUpdate()
+    {
+        foreach (Achieve achieve in _achieves)
+        {
+            CheckAchieve(achieve);
+        }   
+    }
+    void Init()
+    {
+        PlayerPrefs.SetInt("InitAchieve", 1);
+        foreach (Achieve achieve in _achieves)
+        {
+            PlayerPrefs.SetInt(achieve.ToString(), 0);
+        }
+        
+    }
+    
+    private void UnlockCharacter()
+    {
+        for (int i = 0; i < lockCharacter.Length; i++)
+        {
+            string achieveName = _achieves[i].ToString();
+            bool isUnlock = PlayerPrefs.GetInt(achieveName) == 1 ? true : false;
+            lockCharacter[i].SetActive(!isUnlock);
+            unlockCharacter[i].SetActive(isUnlock);
+        }
+    }
+
+    void CheckAchieve(Achieve achieve)
+    {
+        bool isAchieve = false;
+
+        switch (achieve)
+        {
+            case Achieve.UnlockPotato:
+                isAchieve = GameManager.Instance.kill >= 10;
+                break;
+            case Achieve.UnlockBean:
+                isAchieve = GameManager.Instance.gameTime >= GameManager.Instance.maxGameTime;
+                break;
+        }
+
+        if (isAchieve && PlayerPrefs.GetInt(achieve.ToString()) == 0)
+        {
+            PlayerPrefs.SetInt(achieve.ToString(), 1);
+
+            for (int i = 0; i < uiNotice.transform.childCount; i++)
+            {
+                bool isActive = i == (int)achieve;
+                uiNotice.transform.GetChild(i).gameObject.SetActive(isActive);
+            }
+            StartCoroutine(NoticeRoutine());
+        }
+    }
+
+    private IEnumerator NoticeRoutine()
+    {
+        uiNotice.SetActive(true);
+        yield return _wait;
+        uiNotice.SetActive(false);
+    }
+
+}
